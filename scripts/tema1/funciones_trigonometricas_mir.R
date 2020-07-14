61459f8cf0c1c06df74321621cdcc117aacf944f
Mauricio I <mauricio.iturbide14@gmail.com>
2020-07-14 03:11:25 -0500

    Creación de script de funciones trigonométricas

---
+++ scripts/tema1/funciones_trigonometricas_mir.R
@@ -0,0 +1,9 @@
+#Conversión de un ángulo a radianes
+
+#Para convertir 60° a radianes
+#Las funciones trigonométricas en r reciben radianes de parámetros
+cos(60*pi/180)
+sin(60*pi/180)
+tan(pi)
+tan(pi/2)
+
