c52aea5face7e5a4dda87d353d468245a7517865
Mauricio I <mauricio.iturbide14@gmail.com>
2020-07-14 02:53:38 -0500

    Creación de scripts de operaciones y funciones

---
+++ scripts/tema1/04-calculadora_mauricio_iturbide.R
@@ -0,0 +1,18 @@
+#Escribir varias operaciones
+1+1;2*3;6/3
+
+#Operación con paréntesis
+(3+6)*3
+
+#Símbolos en R
+pi
+2*pi
+#Infinito
+Inf
+-Inf
+#Na valor desconocido
+NA
+#Not a Number
+NaN
+#0/0 nos daría un NaN
+0/0
---
+++ scripts/tema1/funciones_matematicas_combinacionales_mauricio_iturbide.R
@@ -0,0 +1,22 @@
+#Funciones matemáticas en R
+sqrt(81)
+
+exp(1)
+
+log(pi)
+
+#logaritmo dado una base determinada
+log(32,2) #Base 2 
+
+abs(-pi)
+
+#####Funciones Combinatorias
+
+#Factorial
+factorial(4)
+factorial(0)
+
+#Función choose con 5 platos y quisiéramos comer 3 de ellos ¿De cuántas formas se podrían comer?
+choose(5,3)
+#Repuesta = 10 formas diferentes
+choose(3,1)
