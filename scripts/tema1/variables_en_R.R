38212b55cc491899180d03cd429792a6c5740dc8
Mauricio I <mauricio.iturbide14@gmail.com>
2020-07-15 04:27:44 -0500

    Creación de script de variables en R

---
+++ scripts/tema1/variables_en_R.R
@@ -0,0 +1,9 @@
+#Definición de una variable
+x = (pi^2)/2 #sintaxis con igual
+y <- cos(pi/4) #sintaxis con flecha
+sin(pi/4) + cos(pi/4) -> z #  Otro tipo de sintaxis con flecha
+
+edad <- 24
+nombre <- 'Mauricio'
+
+pi.4 = 4*pi
