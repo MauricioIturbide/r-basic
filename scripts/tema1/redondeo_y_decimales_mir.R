727e601c4bab3371a0bd0a801572f6961d4f5430
Mauricio I <mauricio.iturbide14@gmail.com>
2020-07-15 03:11:29 -0500

    Creación de script de redondeo y decimales en R

---
+++ scripts/tema1/redondeo_y_decimales_mir.R
@@ -0,0 +1,20 @@
+#N cifras significativas de un número
+print(sqrt(2),6)
+
+#redondear a ciertas cifras significativas
+round(sqrt(2),3)
+
+#Parte entera por defecto (redondea hacia abajo)
+floor(sqrt(2))
+
+#Rendondear hacia arriba
+ceiling(sqrt(2))
+
+#Truncamiento
+trunc(sqrt(2))
+
+#Ver 15 cifras del número sin exponencial
+print(2^50, 15)
+
+round(sqrt(2))
+round(sqrt(2),0)
